ac24b84b712d0a2076cf74387395f11d00a15b5b
JNeigh2 <163676669+JNeigh2@users.noreply.github.com>
2024-05-07 15:54:27 -0400

    Update project4.js

--- project4/project4.js
+++ project4/project4.js
@@ -2,3 +2,10 @@ function clock() {
         	document.getElementById('time').innerHTML = new Date();
 }
 setInterval(clock, 1000);
+function validation() {
+  let x = document.forms["form1"]["info"].value;
+  if (x == "") {
+    alert("You must enter text in the question/concern to submit the form");
+    return false;
+  }
+} 
